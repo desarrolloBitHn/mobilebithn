e04ffe1f0b89f59374e37b871a7e49ccd962d14e
Wenceslao Flores <desarrollo@bit.hn>
2018-06-25 17:01:46 -0600

    25 de Junio de 2018 17:01 pm

--- src/pages/correo/correo.ts
+++ src/pages/correo/correo.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, Nav, ModalController } from 'ionic-angular';
-import { FormBuilder, FormGroup, Validators, FormControl, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl, NgForm, Validators } from '@angular/forms';
 
 /**
  * Generated class for the CorreoPage page.
@@ -497,7 +497,7 @@ export class CorreoPage {
 
   private crearMiForm(){
     return this.formBuilder.group({
-      cuentas: new FormControl(),
+      cuentas: new FormControl(0, Validators.max(500) ),
       duracion: new FormControl(),
       total: new FormControl()
     });
